dbc4dd232d4bc71e9f242c153c2996ece06bda0f
Giyeon Kim <gompu123@gmail.com>
2018-02-09 15:30:33 +0900

    PYGAN 0.0.1 PIP INSTALLABLE

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup_requires = [
+    ]
+
+install_requires = [
+    'numpy==1.14.0',
+    'pandas==0.22.0',
+    'torch==0.3.0.post4',
+    'torchvision==0.2.0',
+    'tqdm==4.19.4'
+    ]
+
+dependency_links = [
+    ]
+
+setup(
+    name                =   'pygan',
+    version             =   '0.0.1',
+    description         =   'GAN-based Data Generator In Python 3',
+    author              =   'Giyeom Kim',
+    author_email        =   'gompu123@gmail.com',
+    url                 =   'https://github.com/gompu123/pygan',
+    install_requires    =   install_requires,
+    packages            =   [ 'pygan' ],
+    keywords            =   [ 'Machine Learning', 'Deep Learning', 'AI', 'GAN', 'Generative Adversarial Networks', 'Data Augmentation'],
+    setup_requires      =   setup_requires,
+    dependency_links    =   dependency_links,
+    classifiers         =   [
+        'Programming Language :: Python :: 3.6'
+    ]
+)
