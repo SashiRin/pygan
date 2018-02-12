4382e84d8c82afad2afa8056ac7a449147b1a708
Giyeon Kim <gompu123@gmail.com>
2018-02-12 16:01:20 +0900

    AUTO UPLOAD

--- pygan/models/models.py
+++ pygan/models/models.py
@@ -1,13 +1,11 @@
 from .gan import GAN
 from .cgan import CGAN
-# from .wgan import WGAN
-# from .wcgan import WCGAN
-# from .dagan import DAGAN
+from .wgan import WGAN
+from .wcgan import WCGAN
 
 model = {
     'GAN': GAN,
     'CGAN': CGAN,
-    # 'WGAN': WGAN,
-    # 'WCGAN': WCGAN,
-    # 'DAGAN': DAGAN
+    'WGAN': WGAN,
+    'WCGAN': WCGAN
 }
